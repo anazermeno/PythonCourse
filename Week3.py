eb3b78cd95fea5a519bf3bfab56f59186f61c7c1
AnaZermeno <anazermeno19@gmail.com>
2020-10-04 22:51:18 -0600

    Added recursion examples

--- Week3.py
+++ Week3.py
@@ -58,6 +58,11 @@ for left in range(7):
 
 # recursion
 
+# def recursive_function(parameters):
+#    if base_case_condition(parameters):
+#        return base_case_value
+#    recursive_function(modified_parameters)
+
 def factorial(n):
     print("Factorial called with " + str(n))
     if n < 2:
@@ -68,3 +73,21 @@ def factorial(n):
     return result
 
 factorial(4)
+
+def sum_positive_numbers(n):
+    # The base case is n being smaller than 1
+    if n < 1:
+        return 0
+
+    # The recursive case is adding this number to
+    # the sum of the numbers smaller than this one.
+    return n + sum_positive_numbers(n-1)
+
+print(sum_positive_numbers(3)) # Should be 6
+print(sum_positive_numbers(5)) # Should be 15
+
+# Week 3 EXAM
+
+
+for x in range(1, 10, 3):
+    print(x)
